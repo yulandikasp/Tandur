c8d40ff2752120672d669a16450688a2972c883b
yulandikasp <68653568+yulandikasp@users.noreply.github.com>
2025-08-09 17:06:37 +0700

    Add files via upload

---
+++ dataConfig.js
@@ -0,0 +1,15 @@
+
+const sampleData = [
+    { komoditas: 'padi', bulan: 'Mei', PPL: 'A', PML: 'X', pengecekan: 'Bisa dilakukan ubinan', perkiraan: '2 minggu' },
+    { komoditas: 'palawija', bulan: 'Juni', PPL: 'B', PML: 'Y', pengecekan: 'Gagal panen/puso', perkiraan: '3 minggu' },
+    { komoditas: 'padi', bulan: 'Juli', PPL: 'A', PML: 'Y', pengecekan: 'Lewat panen', perkiraan: '2 minggu' },
+    { komoditas: 'padi', bulan: 'Agustus', PPL: 'C', PML: 'Z', pengecekan: 'Bisa dilakukan ubinan', perkiraan: '1 minggu' }
+];
+
+const warnaKategori = {
+    'Bisa dilakukan ubinan': '#28a745',
+    'Lewat panen': '#ffc107',
+    'Gagal panen/puso': '#dc3545',
+    'blank': '#6c757d',
+    'mundur bulan berikutnya': '#17a2b8'
+};
